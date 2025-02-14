c510d672c117b03c01eab2066d78866edbdf7ed3
Audrius Pasvenskas <apasvenskas@outlook.com>
2025-02-14 13:00:52 -0800

    added underlines to the pdf file

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 22, 200, 22)
-
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
@@ -20,7 +20,7 @@ for index, row in df.iterrows():
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
-        pdf.ln(275)
+        pdf.ln(276)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
